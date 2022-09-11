d68d15476e601374e5e2c58a89e0476a014ce082
alisa <alisavy2010@gmail.com>
2022-09-11 17:08:03 +0300

    Initial Commit

---
+++ agvyshegorodtsevaPW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  agvyshegorodtsevaPW1
+//
+//  Created by Алиса Вышегородцева on 11.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
